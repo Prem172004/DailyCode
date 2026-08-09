7c25533186862af593e49486b4afa23909879c69
Prem <www.prem172004@gmail.com>
2026-08-10 00:12:50 +0530

    Time: 2 ms (32.44%) | Memory: 42.8 MB (52.56%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((b,a)-> a - b);
+        for(int i : stones) {
+            pq.offer(i);
+        }
+        while(pq.size() > 1) {
+            int stone1 = pq.poll();
+            int stone2 = pq.poll();
+            if(stone1 != stone2) {
+                pq.offer(stone1 - stone2);
+            }
+        }
+        return pq.size() == 0 ? 0 : pq.peek();
+    }
+}
