b8c6de0da1cb81bbbdf74e211735df6e9893f347
Prem <www.prem172004@gmail.com>
2026-08-14 15:31:05 +0530

    Time: 12 ms (1.47%) | Memory: 77.5 MB (19.65%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,12 +1,37 @@
 class Solution {
-    public int maxSubArray(int[] nums) {
-        int best = nums[0];
-        int currentMax = nums[0];
+    int cmax(int[] nums, int left, int mid, int right) {
+        int sum = 0;
+        int leftSum = Integer.MIN_VALUE;
 
-        for (int i = 1; i < nums.length; i++) {
-            currentMax = Math.max(currentMax + nums[i], nums[i]);
-            best = Math.max(currentMax, best);
+        for(int i = mid; i >= left; i--) {
+            sum += nums[i];
+            leftSum = Math.max(leftSum, sum);
         }
-        return best;
+        sum = 0;
+        int rightSum = Integer.MIN_VALUE;
+
+        for(int i = mid + 1; i <= right; i++) {
+            sum += nums[i];
+            rightSum = Math.max(rightSum, sum);
+        }
+
+        return leftSum + rightSum;
+    }
+
+    int solve(int[] nums, int left, int right) {
+        //base
+        if (left == right)
+            return nums[left];
+        int mid = left + (right - left) / 2;
+
+        int leftmax = solve(nums, left, mid);
+        int rightmax = solve(nums, mid + 1, right);
+        int centermax = cmax(nums, left, mid, right);
+
+        return Math.max(leftmax, Math.max(rightmax, centermax));
+    }
+
+    public int maxSubArray(int[] nums) {
+        return solve(nums, 0, nums.length - 1);
     }
 }
