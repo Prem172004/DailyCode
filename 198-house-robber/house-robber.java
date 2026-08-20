952de9cb277490dce1d02a16dc880b683bfe8be0
Prem <www.prem172004@gmail.com>
2026-08-21 04:32:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (38.27%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -8,22 +8,36 @@ class Solution {
 
     //     return Math.max(pick, skip);
     // }
-    int solveMemo(int[] nums, int i, int[] memo) {
-        if(i >= nums.length) return 0;
+    // int solveMemo(int[] nums, int i, int[] memo) {
+    //     if(i >= nums.length) return 0;
 
-        if(memo[i] != -1) return memo[i];
+    //     if(memo[i] != -1) return memo[i];
 
-        int pick = nums[i] + solveMemo(nums, i+2, memo);
-        int skip = solveMemo(nums,i+1, memo);
+    //     int pick = nums[i] + solveMemo(nums, i+2, memo);
+    //     int skip = solveMemo(nums,i+1, memo);
 
+    //     memo[i] = Math.max(pick, skip);
+    //     return memo[i];
+    // }
+    int solveTable(int[] nums) {
+        int[] memo = new int[nums.length+2];
+        Arrays.fill(memo, -1);
+        memo[nums.length] = 0;
+        memo[nums.length+1] = 0;
+
+        for(int i = nums.length-1; i >= 0; i--) {
+        int pick = nums[i] + memo[i+2];
+        int skip = memo[i+1];
         memo[i] = Math.max(pick, skip);
-        return memo[i];
+        }
+        return memo[0];
     }
 
     public int rob(int[] nums) {
         //return solveRec(nums, 0);
-        int[] memo = new int[nums.length+1];
-        Arrays.fill(memo, -1);
-        return solveMemo(nums, 0, memo);
+        //int[] memo = new int[nums.length+1];
+       // Arrays.fill(memo, -1);
+        //return solveMemo(nums, 0, memo);
+        return solveTable(nums);
     }
 }
