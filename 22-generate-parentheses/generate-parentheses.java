c65808bd24a2721d15dbe37fc6442f04943b1599
Prem <www.prem172004@gmail.com>
2026-09-10 23:43:41 +0530

    Time: 1 ms (85.42%) | Memory: 44.1 MB (96.48%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, new StringBuilder(), 0, 0, n);
+        return result;
+    }
+
+    private void backtrack(List<String> result, StringBuilder cur,
+                           int open, int close, int max) {
+        if (cur.length() == max * 2) {
+            result.add(cur.toString());
+            return;
+        }
+        if (open < max) {
+            cur.append('(');
+            backtrack(result, cur, open + 1, close, max);
+            cur.deleteCharAt(cur.length() - 1); 
+        }
+        if (close < open) {
+            cur.append(')');
+            backtrack(result, cur, open, close + 1, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+    }
+}
